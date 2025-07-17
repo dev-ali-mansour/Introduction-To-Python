9d68fb51c7bbcec906bc04dc84914add124386a1
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 10:47:34 +0300

    feat: Add a script demonstrating set and list usage with duplicates

---
+++ 07_set.py
@@ -0,0 +1,5 @@
+# List of rock bands (allows duplicates)
+rock_bands = ["Queen", "Pink Floyd", "Beatles", "Queen"]
+
+# Set of jazz musicians (no duplicates allowed)
+jazz_set = {"Miles Davis", "John Coltrane", "Duke Ellington"}
