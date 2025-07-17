4d617cd1dcc38019a5b1cc59a1acfe6ba06b8045
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 12:06:43 +0300

    feat: Add a script to check dictionary key existence for a workflow example

---
+++ 11_workflow.py
@@ -0,0 +1,10 @@
+books = {"Ahmed Ali": "Introduction To Python", "Sara Saad": "Fundamentals Of Artificial Intelligence",
+         "Ali Mansour": "Fundamentals Of Machine Learning"}
+
+# Check if "Sara Saad" is a key in books
+if "Sara Saad" in books.keys():
+    print(True)
+else:
+    print(False)
+
+
