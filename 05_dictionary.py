3e9735d5d4aaab9553ee78cf2e1abe09b193fdf6
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 10:42:28 +0300

    feat: Enhance the dictionary example with additional operations and printing

--- 05_dictionary.py
+++ 05_dictionary.py
@@ -3,3 +3,12 @@ books={"Ahmed Ali":"Introduction To Python","Sara Saad":"Fundamentals Of Artific
 
 # Print the books
 print(books)
+
+# Print the book by Sara Saad
+print(books["Sara Saad"])
+
+# Add a new book
+books["Ali Mansour"]="Fundamentals Of Machine Learning"
+
+# Print the books in the dictionary
+print(books.values())
