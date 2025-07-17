137426e32c442c61ee0a0df701176cf60ec870e0
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 12:17:00 +0300

    feat: Extend the workflow script with a dictionary key and value condition check

--- 11_workflow.py
+++ 11_workflow.py
@@ -14,3 +14,12 @@ if "Sara Saad" not in books.keys():
     print(True)
 else:
     print(False)
+
+print("==================")
+
+# The and keyword
+book_prices = {"IM92": 45.5, "FD09": 95.45, "ND15": 76.15, "HN13": 64.25}
+if "FD09" in book_prices and min(book_prices.values()) > 60:
+    print(True)
+else:
+    print(False)
