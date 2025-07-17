9871d7a7c236ae06b31265c183409f99abae92cd
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 11:03:51 +0300

    feat: Enhance for loop example to include grade categorization logic

--- 09_for_loop.py
+++ 09_for_loop.py
@@ -4,3 +4,19 @@ degrees = [95.50, 73.25, 82.75, 59.5]
 # Print each value in degrees
 for degree in degrees:
     print(degree)
+
+for degree in degrees:
+    if degree >= 95:
+        print("A+")
+    elif degree >= 90:
+        print("A")
+    elif degree >= 85:
+        print("B+")
+    elif degree >= 80:
+        print("B")
+    elif degree >= 70:
+        print("C+")
+    elif degree >= 60:
+        print("C")
+    else:
+        print("F")
