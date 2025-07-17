81b30421c8fac1b73ea3ac03bc14beb703e340c9
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 11:48:00 +0300

    feat: Add a script demonstrating while loop iteration with a ticket counter

---
+++ 10_while_loop.py
@@ -0,0 +1,11 @@
+# Total tickets available
+tickets = 5
+
+# Number of tickets sold
+tickets_sold = 0
+
+while tickets_sold < tickets:
+    # Increment tickets sold
+    tickets_sold += 1
+    # Print remaining tickets
+    print(tickets - tickets_sold)
