b7b23477fbd0bd3e781e2c6eba10cdb66b1694cb
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 12:23:37 +0300

    feat: Extend the workflow script to filter and collect dictionary keys based on value condition

--- 11_workflow.py
+++ 11_workflow.py
@@ -32,3 +32,18 @@ if "FD09" in book_prices or min(book_prices.values()) > 60:
     print(True)
 else:
     print(False)
+
+print("==================")
+
+# Appending to list
+# Creates an empty list
+expensive_books = []
+# Loop through the dictionary
+for key, val in book_prices.items():
+    # Check if the price is 65 dollars or more
+    if val > 65:
+        # Append the product id to the list
+        expensive_books.append(key)
+
+# Print the list
+print(expensive_books)
