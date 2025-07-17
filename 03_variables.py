c1c841a40f2ba96f49dfb3d44f2d55fc90c405ab
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 10:13:16 +0300

    feat: Add examples of multi-line string usage and printing

--- 03_variables.py
+++ 03_variables.py
@@ -9,3 +9,18 @@ print(student_gpa)
 print(is_passed)
 print(type(student_age))
 print(type(is_passed))
+
+# Multi-line String
+review_one = '''The courses have been fantastic, 
+and their flexibility makes them perfect for my packed schedule.
+The platform has been such a valuable resource.
+I only wish I'd discovered it earlier.
+I'm definitely going to spread the word to my friends about this great experience'''
+
+review_two = '''This time last year, I felt stuck at a career crossroads.
+Fast forward to today, and I've successfully transitioned into a role as a Prompt Engineer.
+I'm incredibly grateful for the impact you've had on my professional journey.
+You're doing amazing work - please continue!'''
+
+print(review_one)
+print(review_two)
