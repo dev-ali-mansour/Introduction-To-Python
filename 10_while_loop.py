c2859a7e7bdaa562891ede215d63661c3cb469da
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 11:56:34 +0300

    feat: Enhance the while loop example to include breaking, conditionals, and ticket status messages

--- 10_while_loop.py
+++ 10_while_loop.py
@@ -9,3 +9,25 @@ while tickets_sold < tickets:
     tickets_sold += 1
     # Print remaining tickets
     print(tickets - tickets_sold)
+
+print("==================")
+
+tickets = 10
+tickets_sold = 0
+# Breaking a loop
+while tickets_sold < tickets:
+    print(tickets - tickets_sold)
+    break
+
+print("==================")
+
+while tickets_sold < tickets:
+    # Increment sold tickets
+    tickets_sold += 1
+    # Conditional statements inside loop
+    if tickets - tickets_sold > 7:
+        print("Plenty of tickets remaining")
+    elif tickets - tickets_sold > 3:
+        print("Some tickets remaining")
+    else:
+        print("Low number of tickets!")
