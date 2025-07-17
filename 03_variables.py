8a09ec34d5ab5c2912336f2256f8da26a74132e9
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-17 10:06:35 +0300

    feat: Add script demonstrating variable usage and type checking

---
+++ 03_variables.py
@@ -0,0 +1,11 @@
+student_age = 23
+student_gpa = 1.25
+is_passed = False
+# Update Student GPA
+student_gpa = 3.75
+is_passed = True
+
+print(student_gpa)
+print(is_passed)
+print(type(student_age))
+print(type(is_passed))
