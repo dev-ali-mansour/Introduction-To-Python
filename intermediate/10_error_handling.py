0e7f383b3a619878b66a044f3c069a3e7264ef9d
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-20 16:21:26 +0300

    feat: Add snake_case function with type checking and error handling

--- intermediate/10_error_handling.py
+++ intermediate/10_error_handling.py
@@ -26,3 +26,18 @@ def average(values):
 
 
 average(sales_dict)
+
+
+def snake_case(text):
+    # Check the data type
+    if type(text) == str:
+        clean_text = text.replace(" ", "_")
+        clean_text = clean_text.lower()
+        return clean_text
+    else:
+        # Return a TypeError error if the wrong data type was used
+        raise TypeError(
+            "The snake_case() function expects a string as an argument, please check the data type provided.")
+
+
+print(snake_case("User Name 187"))
