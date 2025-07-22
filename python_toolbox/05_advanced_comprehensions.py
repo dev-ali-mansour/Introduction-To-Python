98078989654c273e44f3c6ea60f96c840c49b85a
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-22 18:37:36 +0300

    feat: Add dict comprehension example to create a dictionary of fellowship members and their name lengths

--- python_toolbox/05_advanced_comprehensions.py
+++ python_toolbox/05_advanced_comprehensions.py
@@ -44,3 +44,16 @@ new_fellowship = [member if len(member) >= 7 else "" for member in fellowship]
 print(new_fellowship)
 
 print("=======================")
+
+# You are given a list of strings fellowship and, using a dict comprehension, create a dictionary with the members of
+# the list as the keys and the length of each string as the corresponding values.
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create dict comprehension: new_fellowship
+new_fellowship = {member:len(member) for member in fellowship}
+
+# Print the new dictionary
+print(new_fellowship)
+
