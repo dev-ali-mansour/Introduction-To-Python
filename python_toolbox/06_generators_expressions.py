3293885ad03db5776c2b4804a5ddd18dc13a3a7e
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-22 19:10:45 +0300

    feat: Add generator expression example to iterate over string lengths

--- python_toolbox/06_generators_expressions.py
+++ python_toolbox/06_generators_expressions.py
@@ -76,4 +76,19 @@ print(next(result))
 for value in result:
     print(value)
 
+print("========================")
 
+# ou are given a list of strings lannister and, using a generator expression, create a generator object that you will
+# iterate over to print its values.
+
+# Create a list of strings: lannister
+lannister = ['cersei', 'jaime', 'tywin', 'tyrion', 'joffrey']
+
+# Create a generator object: lengths
+lengths = (len(person) for person in lannister)
+
+# Iterate over and print the values in lengths
+for value in lengths:
+    print(value)
+
+print("========================")
