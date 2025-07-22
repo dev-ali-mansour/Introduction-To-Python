70f1b9f8e210c102366a538d51dab128f50d24ee
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-22 18:34:18 +0300

    feat: Add list comprehension example with if-else conditional to filter fellowship members by name length

--- python_toolbox/05_advanced_comprehensions.py
+++ python_toolbox/05_advanced_comprehensions.py
@@ -27,3 +27,20 @@ new_fellowship = [member for member in fellowship if len(member) >= 7]
 
 # Print the new list
 print(new_fellowship)
+
+print("=======================")
+
+# You will work on the same list, fellowship and, using a list comprehension and an if-else conditional statement in the
+# output expression, create a list that keeps members of fellowship with 7 or more characters and replaces others with
+# an empty string. Use member as the iterator variable in the list comprehension.
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member if len(member) >= 7 else "" for member in fellowship]
+
+# Print the new list
+print(new_fellowship)
+
+print("=======================")
