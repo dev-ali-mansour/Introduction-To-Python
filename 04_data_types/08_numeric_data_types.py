c905a5df4d2d0c82537d6ec12670f1687e4e4600
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-27 14:57:36 +0300

    feat: Add examples of numeric data types including integers, floats, and decimals

---
+++ 04_data_types/08_numeric_data_types.py
@@ -0,0 +1,21 @@
+from decimal import Decimal
+
+# Integer
+print(int(123456789123456789))
+
+# Float
+print(float(123456789123456789))
+
+# Decimals
+print(Decimal('123456789123456789'))
+
+# Printing floats
+print(0.00001)
+print(f"{0.00001:f}")
+print(f"{0.0000001:f}")
+print(f"{0.0000001:.7f}")
+
+# Python division types
+print(4 / 2)
+print(4 // 2)  # Floored result
+print(7 // 3)
